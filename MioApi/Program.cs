8523659cad08590f7f8bafd989235fb088c4a06b
Allen Bose <boseallen192@gmail.com>
2026-04-15 14:27:22 +0530

    Added sensitive DB data logging

--- MioApi/Program.cs
+++ MioApi/Program.cs
@@ -3,7 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 Env.Load();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(Environment.GetEnvironmentVariable("MIO_DB_STRING"))
+{
+    options.UseNpgsql(Environment.GetEnvironmentVariable("MIO_DB_STRING"));
+    options.EnableSensitiveDataLogging();
+    options.EnableServiceProviderCaching();
+    options.EnableDetailedErrors();
+}
 );
 
 builder.Services.AddControllers();
